bf07d9998a0bfb4b5ba387797fe40069275b9485
Nurialessa <45092390+Nurialessa@users.noreply.github.com>
2018-12-07 11:56:09 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,19 +2,76 @@
 $(document).ready(function() {
 
 	// Strat editing here
-	$("#Read_more1").click(function(){
-  message = "Not only thanks to its location between Französischer Dom and Deutscher Dom is the Christmas market at Gendarmenmarkt one of the most atmospheric in the city. The market offers a large range of handmade goods crafted by artisans, Christmas food, and entertainment.         Time: 10 am - 10 pm; Admission: 2€; Start: 1st December - 24th December";
+	$("#Read_more1").click(function() {
+  message =
+    "Not only thanks to its location between Französischer Dom and Deutscher Dom is the Christmas market at Gendarmenmarkt one of the most atmospheric in the city. The market offers a large range of handmade goods crafted by artisans, Christmas food, and entertainment.         Time: 10 am - 10 pm; Admission: 2€; Start: 1st December - 24th December";
   $("#answer_text1").text(message);
 });
 
 $("#Read_more2").click(function() {
-  message = "The special feature of this Christmas market is its location: Set up in front of the famous Charlottenburg Palace, about 250 vendors display and sell nostalgic Christmas crafts such as nativity figures and tree decorations. Traditional German Christmas sweets like stollen, spritz biscuits and marzipan loaves are available at the market too. Time: 10 am - 10 pm; Admission: free; Start: 1st December - 24th December";
+  message =
+    "The special feature of this Christmas market is its location: Set up in front of the famous Charlottenburg Palace, about 250 vendors display and sell nostalgic Christmas crafts such as nativity figures and tree decorations. Traditional German Christmas sweets like stollen, spritz biscuits and marzipan loaves are available at the market too. Time: 10 am - 10 pm; Admission: free; Start: 1st December - 24th December";
   $("#answer_text2").text(message);
 });
 
 $("#Read_more3").click(function() {
-  message = "The Christmas market around Kaiser Wilhelm Memorial Church at Breitscheidplatz welcomes its visitors with more than 100 beautifully decorated market stands and Christmas booths as well as 70 fairground rides. Between them, a tall Christmas tree and the church, there is much to see and entertain. Time: 10 am - 10 pm; Admission: free; Start: 1st December - 24th December";
+  message =
+    "The Christmas market around Kaiser Wilhelm Memorial Church at Breitscheidplatz welcomes its visitors with more than 100 beautifully decorated market stands and Christmas booths as well as 70 fairground rides. Between them, a tall Christmas tree and the church, there is much to see and entertain. Time: 10 am - 10 pm; Admission: free; Start: 1st December - 24th December";
   $("#answer_text3").text(message);
+});
+
+$("#Nuremberg").click(function() {
+  message =
+    "Oh no!:/";
+  $("#answer_text4").text(message);
+});
+
+$("#Dresden").click(function() {
+  message =
+    "Yes! Correct!:)";
+  $("#answer_text5").text(message);
+});
+
+$("#Munich").click(function() {
+  message =
+    "Opps no!";
+  $("#answer_text6").text(message);
+});
+
+$("#TheCityHall").click(function() {
+  message =
+    "Yayaya!";
+  $("#answer_text7").text(message);
+});
+
+$("#St_Stephens_Cathedral").click(function() {
+  message =
+    "Oppsieee";
+  $("#answer_text8").text(message);
+});
+
+$("#TheOperaHouse").click(function() {
+  message =
+    "Opps no!";
+  $("#answer_text9").text(message);
+});
+
+$("#Turkey").click(function() {
+  message =
+    "Perfect!";
+  $("#answer_text10").text(message);
+});
+
+$("#Greece").click(function() {
+  message =
+    "Wrong!";
+  $("#answer_text11").text(message);
+});
+
+$("#Italy").click(function() {
+  message =
+    "Nooo";
+  $("#answer_text12").text(message);
 });
 
 	// Stop editing here
